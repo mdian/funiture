49bc349c8422554bd25ad0d24249e71bba759129
mdian <strangeman079@gmail.com>
2021-06-05 11:52:20 +0300

    push until tour section

--- js/plugins.js
+++ js/plugins.js
@@ -1,9 +1,45 @@
-let a = 19;
+/* 1 carousel */
+$('#carousel-1').carousel({
+  interval: 4000,
+  wrap: true,
+  keyboard: true
+});
 
-(function infoked() {
-      a = 100;
-      
-})();
+
+/* 2 carousel */
+$('#carousel-2').carousel({
+  interval: 6000,
+  wrap: true,
+  keyboard: true
+});
+/* 3 carousel */
+$('#carousel-3').carousel({
+  interval: 8000,
+  wrap: true,
+  keyboard: true
+});
+
+/* 4 carousel example with jumbotron */
+$('#carousel-4').carousel({
+  interval: 10000,
+  wrap: true,
+  keyboard: true
+});
 
 
-console.log(a)
+// close icon in tour section
+
+let closeIcon = document.querySelector('.close_toure_icon');
+let modal_tour = document.querySelector('.modal_tour');
+let modal = document.querySelector('.modal');
+closeIcon.addEventListener('click', function () {
+  let modalbackdrop = document.querySelector('.modal-backdrop');
+  modal_tour.style.display = 'none';
+  modalbackdrop.classList.remove('show');
+  modalbackdrop.style.display = "none";
+  modal.classList.remove('show');
+
+
+  // modalbackdrop.style.display = 'none'
+
+})
